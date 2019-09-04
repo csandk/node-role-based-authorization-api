3b39c03957be917200cd724485b982c7ecf71462
csandk <c.sandk@live.de>
2019-09-04 16:36:08 +0200

    lock mechanismus

--- users/user.service.js
+++ users/user.service.js
@@ -5,25 +5,95 @@ const Role = require('_helpers/role');
 // users hardcoded for simplicity, store in a db for production applications
 const users = [
     { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
-    { id: 2, username: 'user', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User }
+    { id: 2, username: 'user', password: 'user', firstName: 'ViewOnly', lastName: 'User', role: Role.User },
+    { id: 3, username: 'editor', password: 'editor', firstName: 'Normal', lastName: 'User', role: Role.Editor }
 ];
+
+var loggedInUserToken = null;
+var editUserTimeout = null;
+const timeoutInMinutes = 2;
 
 module.exports = {
     authenticate,
+    reauthenticate,
     getAll,
-    getById
+    getById,
+    logout
 };
 
 async function authenticate({ username, password }) {
     const user = users.find(u => u.username === username && u.password === password);
+    
     if (user) {
-        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
+        if (user.username == "editor" && loggedInUserToken) {
+            console.log("editor logged in");
+            return "editorBlocked";
+        }
+        const token = jwt.sign({ sub: user.id, role: user.role, exp: Math.floor(Date.now() / 1000) + (60 * timeoutInMinutes) }, config.secret);
         const { password, ...userWithoutPassword } = user;
+        if (user.username == "editor") {
+            loggedInUserToken = token;
+            editUserTimeout = setTimeout(resetToken, timeoutInMinutes * 60000)
+        }        
         return {
             ...userWithoutPassword,
+            timeoutInMinutes,
             token
         };
     }
+}
+
+async function reauthenticate(oldUser) {
+    const user = users.find(u => u.username === oldUser.username);
+    
+    if (user ) {
+        try {
+            jwt.verify(oldUser.token, config.secret)
+        } catch (error) {
+            logIt(error)
+        }        
+        const token = jwt.sign({ sub: user.id, role: user.role, exp: Math.floor(Date.now() / 1000) + (60 * timeoutInMinutes) }, config.secret);
+        const { password, ...userWithoutPassword } = user;
+        if (user.username == "editor" && loggedInUserToken) {
+            clearTimeout(editUserTimeout);
+            loggedInUserToken = token;
+            editUserTimeout = setTimeout(resetToken, timeoutInMinutes * 60000)
+        }                
+        return {
+            ...userWithoutPassword,
+            timeoutInMinutes,
+            token
+        };
+    }
+}
+
+async function logout(oldUser) {
+    const user = users.find(u => u.username === oldUser.username);
+    
+    if (user) {
+        logIt("foundUserToLogout");
+        try {
+            jwt.verify(oldUser.token, config.secret)
+            logIt("verified")
+        } catch (error) {
+            logIt(error)
+        }        
+        if (user.username == "editor" && loggedInUserToken) {
+            clearTimeout(editUserTimeout);
+            loggedInUserToken = null;
+        }                
+        return true;
+    }
+    return false;
+}
+
+function logIt(word) {
+    console.log(word);
+}
+
+function resetToken() {
+    loggedInUserToken = null;
+    console.log("reset Token")
 }
 
 async function getAll() {
--- users/users.controller.js
+++ users/users.controller.js
@@ -6,13 +6,48 @@ const Role = require('_helpers/role');
 
 // routes
 router.post('/authenticate', authenticate);     // public route
+router.post('/reauthenticate', reauthenticate);     // public route
 router.get('/', authorize(Role.Admin), getAll); // admin only
 router.get('/:id', authorize(), getById);       // all authenticated users
+router.post('/logout', logout);       // all authenticated users
 module.exports = router;
 
 function authenticate(req, res, next) {
+    console.log("login")
     userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .then(user => {
+            if (user == "editorBlocked") {
+                res.status(400).json({ message: 'User Editor is currently in use' })
+            } else if (user) {
+                res.json(user)
+            } else {
+                res.status(400).json({ message: 'Username or password is incorrect' })
+            }
+        }//user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' })
+        )
+        .catch(err => next(err));
+}
+
+function reauthenticate(req, res, next) {
+    userService.reauthenticate(req.body)
+        .then(user => {
+            if(user) {
+                res.json(user)
+            } else {
+                res.status(400).json({ message: 'User not found or invalid token' })
+            }
+        })
+        .catch(err => next(err));
+}
+
+function logout(req, res, next) {
+    console.log("logout")
+    userService.logout(req.body)
+        .then(result => {
+            if(result) {
+                res.json(result)
+            }
+        })
         .catch(err => next(err));
 }
 
